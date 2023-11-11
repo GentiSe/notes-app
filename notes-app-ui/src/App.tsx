8d16c08eaa836a98c58948722a319c8e4f5a07ad
GentritSelimi <gentselimi7@gmail.com>
2023-11-11 17:35:02 +0100

    UI implementation

--- notes-app-ui/src/App.tsx
+++ notes-app-ui/src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from 'react';
 
-function App() {
+type Note = {
+  id: number,
+  title:string,
+  content:string
+}
+const App = () => {
+  const [notes,setNotes] = useState<Note[]>([
+{
+  id:1,
+  title:"note title 1",
+  content:"content"
+},
+{
+  id:2,
+  title:"note title 1",
+  content:"content"
+},
+{
+  id:3,
+  title:"note title 1",
+  content:"content"
+},
+{
+  id:4,
+  title:"note title 1",
+  content:"content"
+},{
+  id:5,
+  title:"note title 1",
+  content:"content"
+}
+
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className="note-form">
+        <input placeholder="Title" required />
+        <textarea placeholder="Content" rows={10} required />
+
+        <button type="submit">Add Note</button>
+      </form>
+      <div className="notes-grid">
+        {notes.map((note) => (
+          <div className="note-item">
+          <div className="notes-header">
+            <button>x</button>
+          </div>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
+        </div>
+        ))}
+        
+      </div>
     </div>
   );
-}
+};
 
 export default App;
